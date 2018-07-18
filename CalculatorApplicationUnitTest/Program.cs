25049efbdb4710975ffdb50edd7b677a750d105e
yohanneaayele <yohannesh1@yahoo.com>
2018-07-18 15:19:16 +0200

    this is change done for indentation

--- CalculatorApplicationUnitTest/Program.cs
+++ CalculatorApplicationUnitTest/Program.cs
@@ -5,26 +5,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace CalculatorApplicationUnitTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double x = 6.4;
-            double y = 2.5;
+            double x = 6.4, y = 2.5;
             Console.WriteLine("x value is " + x);
-            Console.WriteLine("y value is " +  y);
+            Console.WriteLine("y value is " + y);
             Calcuator cal = new Calcuator();
-            Console.WriteLine("Addition of x and y is "+cal.Addition(x, y));
+            Console.WriteLine("Addition of x and y is " + cal.Addition(x, y));
             Console.WriteLine("Substraction of x and y is " + cal.Substraction(x, y));
             Console.WriteLine("Multiplication of x and y is " + cal.Multiplication(x, y));
             Console.WriteLine("Division of x and y is " + cal.Division(x, y));
             Console.WriteLine("Modulos  of x and y is " + cal.Modulos(x, y));
-          
-           
-
         }
     }
 }
--- MyCalculator/Calcuator.cs
+++ MyCalculator/Calcuator.cs
@@ -7,53 +7,39 @@ using System.Threading.Tasks;
 namespace MyCalculator
 {
     public class Calcuator
-    {   public double Addition(double x, double y)
+    { 
+        public double Addition(double x, double y)
         {
-            double result;
-            result = x + y;
+            double result = x + y;
             return result;
         }
         public double Substraction(double x, double y)
         {
-            double result;
-            result = Math.Round(( x - y),1);
+            double result = Math.Round((x - y), 1); ;
             return result;
         }
-        public  double Multiplication ( double x, double y)
+        public double Multiplication(double x, double y)
         {
-            double result;
-            result= Math.Round((x *y),1);
+            double result = Math.Round((x * y), 1);
             return result;
-
         }
-        public double Division (double x, double y)
+        public double Division(double x, double y)
         {
             if (y == 0)
             {
                 throw new Exception("Division by zero");
             }
-            double result;
-            result =Math.Round( (x / y),1);
+            double result = Math.Round((x / y), 1);
             return result;
-
         }
-        public double  Modulos (double x, double y)
+        public double Modulos(double x, double y)
         {
-            double result;
-            result = x % y;
+            double result = x % y;
             return result;
         }
         private bool IsNegative(double num)
         {
-            bool mybool;
-            if (num < 0)
-            {
-                mybool = true;
-            }
-            else
-                mybool = false;
-
-            return mybool;
+            return (num < 0);
         }
     }
 }
