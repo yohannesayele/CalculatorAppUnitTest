c00e7f4e46e6b6ab8b768772de6211917c97e197
yohanneaayele <yohannesh1@yahoo.com>
2018-07-18 16:03:23 +0200

    change on the indentation.

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -10,106 +10,72 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestAddition()
         {
-            Calcuator calc =  new Calcuator();
-
+            Calcuator calc = new Calcuator();
             // arrange
-            double x = 5.6;
-            double y = 3.2;
-            double expected = 8.8;
-            
+            double x = 5.6, y = 3.2, expected = 8.8;
             // act
-           double  result = calc.Addition(x, y);
-
+            double result = calc.Addition(x, y);
             // assert
-
-            Assert.AreEqual(expected,result);
+            Assert.AreEqual(expected, result);
         }
 
         [TestMethod]
         public void TestSubstraction()
         {
             Calcuator calc = new Calcuator();
-
             // arrange
-            double x = 6.4;
-            double y = 4.2;
-            double expected = 2.2;
-
+            double x = 6.4, y = 4.2, expected = 2.2;
             // act
             double result = calc.Substraction(x, y);
-
             // assert
-
-            Assert.AreEqual(expected,result);
+            Assert.AreEqual(expected, result);
         }
         [TestMethod]
         public void TestMultiplication()
         {
             Calcuator calc = new Calcuator();
-
             // arrange
-            double x = 6.0;
-            double y = 4.2;
-            double expected = 25.2;
-
+            double x = 6.0, y = 4.2, expected = 25.2;
             // act
             double result = (calc.Multiplication(x, y));
-
             // assert
-
-            Assert.AreEqual(expected,result);
+            Assert.AreEqual(expected, result);
         }
         [TestMethod]
         public void TestDivision()
         {
             Calcuator calc = new Calcuator();
-
             // arrange
-            double x = 6.8;
-            double y = 4.2;
-            double expected = 1.6;
-
+            double x = 6.8, y = 4.2, expected = 1.6;
             // act
             double result = calc.Division(x, y);
-
             // assert
-
-            Assert.AreEqual(expected,result);
+            Assert.AreEqual(expected, result);
         }
         [TestMethod]
         public void TestModulos()
         {
             Calcuator calc = new Calcuator();
-
             // arrange
             double x = 6.0;
             double y = 4.0;
             double expected = 2;
-
             // act
             double result = calc.Modulos(x, y);
-
             // assert
-
-            Assert.AreEqual(expected,result);
+            Assert.AreEqual(expected, result);
         }
         [TestMethod]
         public void TestIsNegativeNumber()
         {
             PrivateObject cal = new PrivateObject(typeof(Calcuator));
             Calcuator calc = new Calcuator();
-
             // arrange
             double x = -6.0;
             bool expected = true;
-           
-
-
             // act
-              bool result =Convert.ToBoolean( cal.Invoke("IsNegative", x));
-
+            bool result = Convert.ToBoolean(cal.Invoke("IsNegative", x));
             // assert
-
             Assert.AreEqual(result, expected);
         }
     }
